d7457ecf66bcef9da2be2dc313b93c1eac336a5b
ruxandrantonia <antonia.constantin22@gmail.com>
2016-07-07 08:41:48 +0300

    Jsregister

--- Qshop-master/app/register/register.js
+++ Qshop-master/app/register/register.js
@@ -1,4 +1,4 @@
-function registerButton(){
+/*function registerButton(){
 
      var user=document.querySelector('#username').value
      var pass=document.querySelector('#password').value
@@ -17,4 +17,49 @@ function validate(){
     inputvalue.style.border=" 1px solid green"
   }
 
-  }
+} */
+
+document.addEventListener('DOMContentLoaded',function(){
+    var registerForm = document.querySelector('#register');
+    var inputs = ['#username','#email','#password','#confirm-password'];
+    for(var i=0;i<inputs.length;i++) {
+      document.querySelector(inputs[i]).addEventListener("keyup",function() {
+        onchangeElem('#' + this.id)
+      })
+    }
+    registerForm.addEventListener('submit', function(event){
+        var hasError = false;
+        for(var i=0;i<inputs.length;i++) {
+          if(isEmptyInput(inputs[i])) {
+             hasError = true;
+             showError(inputs[i])
+          } else {
+            hideError(inputs[i])
+          }
+        }
+
+        if(hasError) {
+          event.preventDefault();
+        }
+    })
+})
+
+function onchangeElem(inputId) {
+  isEmptyInput(inputId) ? showError(inputId) : hideError(inputId);
+}
+
+function showError(inputId) {
+  var parentNode =  document.querySelector(inputId).parentNode;
+  parentNode.className +=  ' has-error';
+  parentNode.querySelector('.help-block').className = parentNode.querySelector('.help-block').className.replace('hidden','');
+}
+function hideError(inputId) {
+  var parentNode =  document.querySelector(inputId).parentNode;
+  parentNode.className = parentNode.className.replace('has-error','');
+
+  parentNode.querySelector('.help-block').className = parentNode.querySelector('.help-block').className.replace('hidden','');
+  parentNode.querySelector('.help-block').className +=' hidden';
+}
+function isEmptyInput(inputId) {
+  return !document.querySelector(inputId).value
+}
